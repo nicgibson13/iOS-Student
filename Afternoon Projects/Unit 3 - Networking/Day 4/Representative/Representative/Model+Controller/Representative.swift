78599ccf847d0c0e42b8541bcb30ddc8442072d3
Nic Gibson <nic@shpangy.com>
2019-06-27 13:39:24 -0600

    Created Folders, Setup Project

---
+++ Afternoon Projects/Unit 3 - Networking/Day 4/Representative/Representative/Model+Controller/Representative.swift
@@ -0,0 +1,13 @@
+//
+//  Representative.swift
+//  Representative
+//
+//  Created by Nic Gibson on 6/27/19.
+//  Copyright © 2019 DevMtnStudent. All rights reserved.
+//
+
+import Foundation
+
+struct Representative {
+    
+}
